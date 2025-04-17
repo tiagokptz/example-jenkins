aeed66f255ee6c9b8028638d0098f9e038afeadf
tiago <tiagoeliassilva2005@gmail.com>
2025-04-16 21:50:17 -0300

    testando integracao jenkins

--- src/main/java/br/edu/catolica/example_jenkins/controller/HelloController.java
+++ src/main/java/br/edu/catolica/example_jenkins/controller/HelloController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/hello")
 public class HelloController {
 
+    //endpoint GET
     @GetMapping("/{name}")
     public String helloUser(@PathVariable("name") String name) {
         return "Seja bem-vindo(a) " + name;
